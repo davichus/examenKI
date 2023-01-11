e4eae7293ef0fb7a37bdeb151bbf279d162e2b54
davidnavarrete <davidnava1998efrain@outlook.com>
2023-01-10 20:08:21 -0500

    VERDISON FINAL
    FINAL

---
+++ kIzaExamen1/Registro.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kIzaExamen1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(String usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double montoInicial = Convert.ToDouble(txtMonto.Text);
+                double Pago = (4000 - montoInicial) / 5 + 5 % 4000;
+
+                txtPago.Text = Pago.ToString();
+
+            }
+            catch (Exception ex)
+            {
+
+                DisplayAlert("Alerta", ex.ToString(), "Cerrar");
+
+            }
+          
+
+        }
+
+        private void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Resumen(lblUsuario.Text, txtNombre.Text, txtPago.Text));
+        }
+    }
+}
---
+++ kIzaExamen1/Resumen.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace kIzaExamen1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen(string usuario, string nombre, string total)
+        {
+            InitializeComponent();
+            lblUsuario.Text = usuario;
+            txtNombre.Text = nombre;
+            txtTotal.Text = total;
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void btnAcerca_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Realizado por ", "KAROL ALEJANDRA IZA LOACHAMIN", "ACEPTAR");
+        }
+    }
+}
